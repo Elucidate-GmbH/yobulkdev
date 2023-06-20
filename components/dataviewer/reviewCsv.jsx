5a9429c1590d374cba0688f02cb01984bc0888da
Bogdan Mihaileanu <mihaileanu.bogdan@yahoo.com>
2023-06-20 18:35:19 +0300

    event to close importer on finish upload

--- components/dataviewer/reviewCsv.jsx
+++ components/dataviewer/reviewCsv.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useCallback, useEffect, useContext} from 'react';
 import FileDownload from 'js-file-download';
-import axios from 'axios';
+import axios from '../../lib/axios-instance';
 import HappyModal from './happyModal';
 import { CloudArrowDownIcon } from '@heroicons/react/24/outline';
 import ErrorTypeDropDown from './errorTypeSelector';
@@ -8,6 +8,7 @@ import WarningModal from './warningModal';
 import { Switch } from '@headlessui/react';
 import SuccessModal from './SuccessModal';
 import { FaMagic } from 'react-icons/fa';
+import { Context } from '../../context';
 
 const ReviewCsv = ({
   collectionName,
@@ -91,7 +92,10 @@ const ReviewCsv = ({
       setShowResultModal(true);
 
       setTimeout(() => {
-        window.top.postMessage({ eventType: "closeImporter" }, state.coEvents.origin)
+        window.top.postMessage(
+          { eventType: "closeImporter" },
+          state.efiOrigin
+        )
       }, 2000)
 
       return;
