befe648850b9675d44d23e3e9f7c843fb3c14447
Bogdan Mihaileanu <mihaileanu.bogdan@yahoo.com>
2023-06-29 14:11:01 +0300

    Update index.js

--- pages/api/upload/index.js
+++ pages/api/upload/index.js
@@ -17,13 +17,19 @@ let bucketFilePath = null
 let bucketName = null
 function tempDiskFilePath (filename) {
   return `./tmpFiles/${filename}`;
-} 
+}
 function generateRandomString () {
-  return crypto.randomBytes(10).toString('base64').replace(/[=/+]/g, '');
+  let ret = crypto.randomBytes(10).toString('base64').replace(/[=/+]/g, '');
+  console.log('generateRandomString for bucket file', ret);
+  return ret
+
 }
 
 async function saveFile (file, filename) {
   await new Promise(resolve => {
+    if (!fs.existsSync('./tmpFiles')) {
+      fs.mkdirSync('./tmpFiles', { recursive: true });
+    }
     var fstream = fs.createWriteStream(tempDiskFilePath(filename));
     file.pipe(fstream);
     fstream.on('finish', async () => resolve())
@@ -37,22 +43,23 @@ async function uploadFile(filename) {
 
   if (!projectId || !bucketName) {
     bucketFilePath = null
-    await deleteTempFile(filename)
+    await deleteTempFile(filename, 'uploade file 71')
     return console.log('GCS project id or bucket name was not provided')
   }
   const storage = new Storage({ projectId });
-
   try {
+    console.log('before bucket storage', filename);
     await storage.bucket(bucketName).upload(tempDiskFilePath(filename), { destination: `${bucketDestination}${filename}` });
   }
   catch (e) {
     bucketFilePath = null
-    await deleteTempFile(filename)
+    await deleteTempFile(filename, 'upload file error')
     console.log('copy file to bucket error', e)
   }
 }
 
-async function deleteTempFile (filename) {
+async function deleteTempFile (filename, from) {
+  console.log('deleteTempFile from ', from);
   fs.unlinkSync(tempDiskFilePath(filename));
 }
 export const config = {
@@ -87,84 +94,91 @@ async function processUpload(req) {
     const dbClient = new StreamToMongoDB(dbConfig);
     const db = client.db(process.env.DATABASE_NAME | 'yobulk');
 
-    db.collection('templates').findOne(
-      { _id: ObjectId(req.headers.template_id) },
-      function (err, results) {
-        if (err) throw err;
-        else {
-          busboy.on(
-            'file',
-            async function (fieldname, file, filename, encoding, mimetype) {
-              bucketFilePath = '/' + bucketDestination + filename.filename
-              await saveFile(file, filename.filename)
+    let results;
+    console.log('req.headers.template_id', req.headers.template_id);
+    try {
+      results = await db.collection('templates').findOne({ _id: ObjectId(req.headers.template_id) });
+      if (!results) {
+        reject(new Error('No matching template found'));
+        return;
+      }
+    } catch (err) {
+      reject(err);
+      return;
+    }
+    busboy.on(
+      'file',
+      async function (fieldname, file, filename, encoding, mimetype) {
+        console.log('busboy on file, fieldname: ', fieldname, ' filename ', filename);
+        bucketFilePath = '/' + bucketDestination + filename.filename
+        await saveFile(file, filename.filename)
+        console.log('saved file ', filename.filename, ' in ', bucketFilePath);
+        const readableStream = fs.createReadStream(tempDiskFilePath(filename.filename));
 
-              const readableStream = fs.createReadStream(tempDiskFilePath(filename.filename));
-
-              pipeline(
-                readableStream,
-                openCsvInputStream,
-                headers_changes,
-                datatype_validate,
-                dbClient.stream,
-                async (err) => {
-                  if (err) {
-                    console.log('Pipeline failed with an error:', err);
-                  } else {
-                    await deleteTempFile(filename.filename)
-                    console.log('Pipeline ended successfully');
-                  }
-                }
-              );
-
-              file.on('end', function () {
-                db.collection('templates')
-                  .updateOne(
-                    { _id: ObjectId(req.headers.template_id) },
-                    { $set: { collection_name: collectionName } },
-                    { upsert: true }
-                  )
-                  .then((result, err) => {
-                    resolve({ collection_name: collectionName, filePath: bucketFilePath });
-                  })
-                  .catch((err) => {
-                    console.log(err);
-                    reject({ collection_name: collectionName, filePath: bucketFilePath });
-                  });
-              });
+        pipeline(
+          readableStream,
+          openCsvInputStream,
+          headers_changes,
+          datatype_validate,
+          dbClient.stream,
+          async (err) => {
+            if (err) {
+              console.log('Pipeline failed with an error:', err);
+            } else {
+              await deleteTempFile(filename.filename, 'where its supposed to')
+              console.log('Pipeline ended successfully');
             }
-          );
-        }
-        busboy.on('close', () => resolve({ collection_name: collectionName, filePath: bucketFilePath }));
-
-        var headers_changes = new Transform({
-          readableObjectMode: true,
-          writableObjectMode: true,
+          }
+        );
+        file.on('end', function () {
+          console.log('file.on end');
+          db.collection('templates')
+            .updateOne(
+              { _id: ObjectId(req.headers.template_id) },
+              { $set: { collection_name: collectionName } },
+              { upsert: true }
+            )
+            .then((result, err) => {
+              resolve({ collection_name: collectionName, filePath: bucketFilePath });
+            })
+            .catch((err) => {
+              console.log(err);
+              reject({ collection_name: collectionName, filePath: bucketFilePath });
+            });
         });
-
-        headers_changes._transform = async function (data, enc, cb) {
-          var newdata = await transformer({
-            data,
-            transformArrSchema: results.columns,
-          });
-          headers_changes.push(newdata);
-          cb();
-        };
-
-        var datatype_validate = new Transform({
-          readableObjectMode: true,
-          writableObjectMode: true,
-        });
-
-        let ajv = ajvCompileCustomValidator({ template: results });
-        datatype_validate._transform = function (data, enc, cb) {
-          var newdata = dataValidate({ data, colSchema: results, ajv });
-          datatype_validate.push(newdata);
-          cb();
-        };
-
-        req.pipe(busboy);
       }
     );
+    busboy.on('close', () => {
+      console.log('busboy on close');
+      resolve({ collection_name: collectionName, filePath: bucketFilePath });
+    });
+    busboy.on('error', function(err) {
+      console.log('Error in busboy:', err);
+    });
+
+    var headers_changes = new Transform({
+      readableObjectMode: true,
+      writableObjectMode: true,
+    });
+    headers_changes._transform = async function (data, enc, cb) {
+      var newdata = await transformer({
+        data,
+        transformArrSchema: results.columns,
+      });
+      headers_changes.push(newdata);
+      cb();
+    };
+    var datatype_validate = new Transform({
+      readableObjectMode: true,
+      writableObjectMode: true,
+    });
+    let ajv = ajvCompileCustomValidator({ template: results });
+    datatype_validate._transform = function (data, enc, cb) {
+      var newdata = dataValidate({ data, colSchema: results, ajv });
+      datatype_validate.push(newdata);
+      cb();
+    };
+    req.pipe(busboy);
   });
 }
 
@@ -172,6 +186,8 @@ export default async function csvUploadHandler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).end();
   }
+
   const returnValue = await processUpload(req);
+  console.log('~~~~~return value~~~~~', returnValue);
   res.status(200).end(JSON.stringify(returnValue));
 }
