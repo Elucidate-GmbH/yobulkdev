d41e8e83204bfea52770fc8508f479138d9b0174
Bogdan Mihaileanu <mihaileanu.bogdan@yahoo.com>
2023-07-18 18:51:02 +0300

    update spinner class and styling

--- components/efi/Spinner.jsx
+++ components/efi/Spinner.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import styles from './Spinner.module.css';
 
-const Spinner = ({ ready }) => {
+const Spinner = ({ width = '80px', height = '80px', position = 'absolute', borderWidth = '6px', color = '#1E40AFFF' }) => {
   return (
-    <span className={`${styles.loader}`} />
+    <span className={`${styles.loader}`} style={{ height, width, position, borderWidth, borderBottomColor: color }} />
   );
 };
 
